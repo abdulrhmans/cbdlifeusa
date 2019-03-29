16c2cdae6d2234535a06dcfadd7ed6c414d4f4c3
Jeff Spry <jeff.spry@teamlocal.org>
2019-03-29 14:47:33 -0500

    added modules

---
+++ emipro_theme_base/model/product_pricelist_item.py
@@ -0,0 +1,6 @@
+from odoo import api, fields, models
+    
+class product_pricelist_item(models.Model):
+    _inherit = "product.pricelist.item"
+    
+    offer_msg = fields.Char(string="Offer Message")
